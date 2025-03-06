6e5ec6d94cfc5345b340e2588ef7e2b7a7400dfd
frankm12 <reyapk234@gmail.com>
2025-03-06 18:52:42 -0400

    Add project files.

---
+++ Prueba_constructores/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prueba_constructores
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Persona persona = new Persona();
+            persona.nombre = "Franklyn";
+            persona.edad = 16;
+            Console.WriteLine($" El nombre de la persona es {persona.nombre} y su edad es {persona.edad}");
+
+            Persona persona2 = new Persona();
+            persona2.nombre = "Railyng";
+            persona2.edad = 14;
+            Console.WriteLine($" El nombre de la persona es {persona2.nombre} y su edad es {persona2.edad}");
+
+            Persona persona3 = new Persona();
+            persona3.nombre = "John";
+            persona3.edad = 15;
+            Console.WriteLine($" El nombre de la persona es {persona3.nombre} y su edad es {persona3.edad}");
+            //Cuando instancio aqui estoy haciendo un objeto llamado animal tipo animal
+            Animales animales = new Animales("Perro","Pitbull");
+            Console.WriteLine($" El animal es un/una {animales.Animal} y su raza es {animales.Raza}");
+
+        }
+    }
+    public class Persona
+    {
+        public string nombre {  get; set; }
+        public int edad {  get; set; }
+    }
+    public class Animales
+    {
+        public string Animal;
+        public string Raza;
+        //Aqui hice el constructor con parametros para despues igualar mis variables a las propiedades de mi clase
+        public Animales(string animal, string raza)
+        {
+            Animal = animal;
+            Raza = raza;
+        }
+    }
+}
